bc65e21e8d2b80ae76a868c7b3d53c97e8c850db
stac1744 <128981646+stac1744@users.noreply.github.com>
2025-09-15 00:56:54 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig(({ mode }) => {
   return {
     base: '/vehiclebuyer/',
     define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     },
     resolve: {
